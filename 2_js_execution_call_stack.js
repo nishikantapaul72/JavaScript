c12bd878edc38d28ed018dc8b877abf2c6b93a69
Nishi Kanta Paul <nishi.kanta@brainstation-23.com>
2025-03-25 12:15:59 +0600

    Execution Context and How JavaScript is Executed

---
+++ 2_js_execution_call_stack.js
@@ -0,0 +1,13 @@
+var num = 2;
+
+function square(number) {
+  return number * number;
+}
+
+var square2 = square(num);
+var square4 = square(4);
+
+console.log(num); // 2
+console.log(square2); // 4
+console.log(square4); // 16
+console.log("Done!"); // Done!
